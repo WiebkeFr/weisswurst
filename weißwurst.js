328cf55eee3ae6b37c6af7ef5f924cabe91ff536
Wiebke Freitag <wiebke.freitag@satellytes.com>
2020-10-15 17:46:47 +0200

    updated meal-lists

--- weißwurst.js
+++ weißwurst.js
@@ -1,5 +1,5 @@
 
-const handleChange=(id)=>{
+const handleChange=(id) => {
     console.log(id)
     const number = document.getElementById("button-number").innerText
     if(id === "button-sub"){
@@ -15,8 +15,23 @@ const handleChange=(id)=>{
 const changeList = (id) => {
     console.log(id)
     if(id === "Wurstliebhaber"){
-
+        document.getElementById("mealList2").style.display = "none"
+        document.getElementById("mealList1").style.display = "block"
     }else{
-
+        document.getElementById("mealList1").style.display = "none"
+        document.getElementById("mealList2").style.display = "block"
     }
+}
+
+const placeOrder = () => {
+    const order = {}
+    order.name = document.getElementById("name-input").value
+    order.email = document.getElementById("email-input").value
+    order.meal = document.getElementById("Wurstliebhaber").checked ? "mealList1" : "mealList2"
+
+    document.getElementById("addingNewOrder").style.display = "none"
+}
+
+const openNewOrder = () => {
+    document.getElementById("addingNewOrder").style.display = "block"
 }
